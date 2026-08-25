f92ca411bc774c786eec5406b77350c20efa0b90
Joshua Caleb Labayne <111472866+JoshuaLabayne@users.noreply.github.com>
2026-08-25 19:12:18 +0800

    Update Entourage.jsx

--- client/src/pages/Entourage.jsx
+++ client/src/pages/Entourage.jsx
@@ -95,7 +95,7 @@ function Entourage() {
 
           <div className="entourage-group">
             <h2>Bible Bearer</h2>
-            <p>Alexander Floro</p>
+            <p>Nevan Dela Cruz</p>
           </div>
 
           <div className="entourage-group">
